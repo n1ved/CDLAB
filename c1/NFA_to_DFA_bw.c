151c85404d4e4824c4404330aed2c66ec1dd7a41
Nived R S <nivedrsalini@gmail.com>
2025-10-02 22:36:35 +0530

    c1p3 : day 4 it works git add .

--- c1/NFA_to_DFA_bw.c
+++ c1/NFA_to_DFA_bw.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
 #define MAX_SIZE 10
 #define PREDEF_SYM 2
@@ -34,30 +33,15 @@ int dequeue(){
     return pending_queue[pq_front++];
 }
 
-void init_nfa(){
+void init(){
     for(int state=0 ; state<n_states ; state++){
         for(int sym=0 ; sym<n_symbol ; sym++){
             nfa_trans[state][sym].count = 0;
         }
     }
-}
 
-void sort_nfa(){
-    for(int st =0 ; st<n_states ; st++){
-        for(int sy=0 ; sy<n_symbol ; sy++){
-            int count = nfa_trans[st][sy].count;
-            if(count){
-                for(int i=0 ; i<count ; i++){
-                    for(int j=0 ; j < count - i - 1 ; j++){
-                        if(nfa_trans[st][sy].states[j] > nfa_trans[st][sy].states[j+1]){
-                            int temp = nfa_trans[st][sy].states[j];
-                            nfa_trans[st][sy].states[j] = nfa_trans[st][sy].states[j+1];
-                            nfa_trans[st][sy].states[j+1] = temp;
-                        }
-                    }
-                }
-            }
-        }
+    for(int i=0 ; i<MAX_SIZE ; i++){
+        dfa_map[i].count = 0;
     }
 }
 
@@ -73,6 +57,16 @@ void bubble_sort(int size , int array[size]){
     }
 }
 
+void sort_nfa(){
+    for(int st =0 ; st<n_states ; st++){
+        for(int sy=0 ; sy<n_symbol ; sy++){
+            int count = nfa_trans[st][sy].count;
+            if(count){
+                bubble_sort(count , nfa_trans[st][sy].states);
+            }
+        }
+    }
+}
 
 bool dup_dfa_state(int st , int sy){
     int count = nfa_trans[st][sy].count;
@@ -100,19 +94,13 @@ void map_dfa_states(){
     enqueue(0);
     for(int st=0 ; st<n_states ; st++){
         for(int sy=0 ; sy<n_symbol ; sy++){
-            if(nfa_trans[st][sy].count || !dup_dfa_state(st, sy)){
+            if(nfa_trans[st][sy].count && !dup_dfa_state(st, sy)){
                 dfa_map[dfa_map_ptr].count = 0;
                 for(int ct=0 ; ct<nfa_trans[st][sy].count ; ct++){
                     dfa_map[dfa_map_ptr].states[dfa_map[dfa_map_ptr].count++] = nfa_trans[st][sy].states[ct];
                 }
                 enqueue(dfa_map_ptr++);
-                printf("Generated new set : ");
-                for(int i=0 ; i<dfa_map[dfa_map_ptr-1].count ; i++){
-                    printf("%d,",dfa_map[dfa_map_ptr-1].states[i]);
-                }
-                printf("\b\n");
             }
-
         }
     }
 }
@@ -134,17 +122,19 @@ void print_nfa(){
 void print_dfa(){
     for(int state=0 ; state<dfa_map_ptr ; state++){
         for(int sym=0 ; sym<n_symbol ; sym++){
+            printf("q{");
+            for(int to_i = 0 ; to_i<dfa_map[state].count ; to_i++){
+                printf("%d,",dfa_map[state].states[to_i]);
+            }
+            printf("\b}\t\t%d\t\t{",sym);
             if(dfa_trans[state][sym].count){
-                printf("q{");
-                for(int to_i = 0 ; to_i<dfa_map[state].count ; to_i++){
-                    printf("%d,",dfa_map[state].states[to_i]);
-                }
-                printf("\b}\t\t%d\t\t{",sym);
                 for(int to_i = 0 ; to_i <dfa_trans[state][sym].count ; to_i++){
                     printf("%d,",dfa_trans[state][sym].states[to_i]);
                 }
-                printf("\b}\n");
+            }else{
+                printf("- ");
             }
+             printf("\b}\n");
         }
     }
 }
@@ -166,15 +156,6 @@ bool dup_in_dfa_map(int size , int array[size]){
     return false;
 }
 
-void print_map(){
-    for(int dfa =0 ; dfa<dfa_map_ptr ; dfa++){
-        printf("DFA STATE %d : {",dfa);
-        for(int i=0 ; i<dfa_map[dfa].count ; i++){
-            printf("%d,",dfa_map[dfa].states[i]);
-        }
-        printf("\b}\n");
-    }
-}
 
 bool in_array_duplicate_found(int size , int array[size] , int element){
     for(int i=0 ; i<size ; i++){
@@ -186,15 +167,10 @@ bool in_array_duplicate_found(int size , int array[size] , int element){
 
 void nfa_to_dfa_resolver(){
     while(pq_rear >= pq_front){
-        printf("Processing QUEUE : ");
-        for(int i = pq_front ; i<pq_rear ; i++)
-            printf(" %d" , pending_queue[i]);
-        printf("\n");
-        print_map();
         int current_dfa_state = dequeue();
         StateSet current_set = dfa_map[current_dfa_state];
         for(int sy = 0 ; sy<n_symbol ; sy++){
-            int new_state[MAX_SIZE] = {0};
+            int new_state[MAX_SIZE] = {-1};
             int new_state_count = 0;
             for(int dst = 0 ; dst < current_set.count ; dst++){
                 int state_in_nfa = current_set.states[dst];
@@ -206,19 +182,17 @@ void nfa_to_dfa_resolver(){
                 }
             }
             bubble_sort(new_state_count, new_state);
-            if(new_state_count && !dup_in_dfa_map(new_state_count, new_state)){
-                printf("Current new state : q{");
-                for(int i=0 ; i<new_state_count ; i++)
-                    printf("%d,",new_state[i]);
-                printf("\b}\n");
+            if(new_state_count ){
                 dfa_trans[current_dfa_state][sy].count = new_state_count;
-                dfa_map[dfa_map_ptr++].count = new_state_count;
+                if (!dup_in_dfa_map(new_state_count, new_state))
+                    dfa_map[dfa_map_ptr++].count = new_state_count;
                 for(int i=0 ; i<new_state_count ; i++){
                     dfa_trans[current_dfa_state][sy].states[i] = new_state[i];
-                    dfa_map[dfa_map_ptr].states[i] = new_state[i];
+                    if (!dup_in_dfa_map(new_state_count, new_state))
+                        dfa_map[dfa_map_ptr-1].states[i] = new_state[i];
                 }
-                enqueue(dfa_map_ptr++);
-                print_map();
+                if (!dup_in_dfa_map(new_state_count, new_state))
+                    enqueue(dfa_map_ptr-1);
             }
         }
     }
@@ -232,7 +206,7 @@ int main(){
     printf("Enter number of transitions : ");
     scanf("%d",&n_nfa_tr);
 
-    init_nfa();
+    init();
 
     printf("Enter transitions\n");
     for(int s_idx = 0 ; s_idx < n_nfa_tr ; s_idx++){
@@ -242,10 +216,14 @@ int main(){
         nfa_trans[from][sym].count++;
     }
 
+    printf("\nNFA TRANSTION TABLE\n");
     print_nfa();
     sort_nfa();
     map_dfa_states();
     nfa_to_dfa_resolver();
+    printf("\nDFA TRANSTION TABLE\n");
     print_dfa();
+
+    printf("{-} denotes dead states\n");
     return 0;
 }
